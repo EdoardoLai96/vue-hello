901dd7e57c953765b4c57a6417cfca8af511cda3
Edoardo Lai <edoardolai96@gmail.com>
2022-02-09 15:46:06 +0100

    Esercizio completato

---
+++ main.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello !',
+      image: "https://upload.wikimedia.org/wikipedia/commons/9/9a/Gull_portrait_ca_usa.jpg",
+      capiton: "Immagine di un gabbiano"
+    }
+  })
